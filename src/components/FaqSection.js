e959aa41adea6418556dcf6c64436d858dd83d0a
guus <ghligtvoet@gmail.com>
2022-06-03 17:47:47 +0200

    add faq section

---
+++ src/components/FaqSection.js
@@ -0,0 +1,37 @@
+import React from "react";
+import home1 from "../img/home1.png";
+
+const FaqSection = () => {
+    return (
+        <div className="faq">
+            <h2>    
+                Any Questions <span>FAQ</span>
+            </h2>
+            <div className="question">
+                <h4>How Do I Start?</h4>
+                <div className="answer">
+                    <p>Dolor autem neque in laborum sapiente qui quibusdamx </p>
+                    <p>Amet ipsum nesciunt corrupti odio adipisci Maxime eaque voluptas at repellendus laudantium perspiciatis eligendi perferendis? Vel voluptatem maxime quisquam nostrum nam modi. Voluptas asperiores labore tenetur repellat iusto Expedita asperiores. </p>
+                </div>
+            </div>
+            <div className="question">
+                <h4>Daily Schedule</h4>
+                <div className="answer">
+                    <p>Dolor autem neque in laborum sapiente qui quibusdamx </p>
+                    <p>Amet ipsum nesciunt corrupti odio adipisci Maxime eaque voluptas at repellendus laudantium perspiciatis eligendi perferendis? Vel voluptatem maxime quisquam nostrum nam modi. Voluptas asperiores labore tenetur repellat iusto Expedita asperiores. </p>
+                </div>
+            </div>
+            <div className="question">
+                <h4>What else?</h4>
+                <div className="answer">
+                    <p>Dolor autem neque in laborum sapiente qui quibusdamx </p>
+                    <p>Amet ipsum nesciunt corrupti odio adipisci Maxime eaque voluptas at repellendus laudantium perspiciatis eligendi perferendis? Vel voluptatem maxime quisquam nostrum nam modi. Voluptas asperiores labore tenetur repellat iusto Expedita asperiores. </p>
+                </div>
+            </div>
+        </div>
+    )
+
+};
+
+export default FaqSection;
+
