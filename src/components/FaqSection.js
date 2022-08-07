7afd74f5c0be28f6afcf9ad167389d3c74c34f66
guus <ghligtvoet@gmail.com>
2022-08-07 15:49:21 +0200

    styling faq section

--- src/components/FaqSection.js
+++ src/components/FaqSection.js
@@ -1,10 +1,11 @@
 import React from "react";
+import styled from "styled-components";
 import home1 from "../img/home1.png";
 import {About, Description, Image, Hide} from "../styles";
 
 const FaqSection = () => {
     return (
-        <About>
+        <Faq>
             <h2>    
                 Any Questions <span>FAQ</span>
             </h2>
@@ -14,6 +15,7 @@ const FaqSection = () => {
                     <p>Dolor autem neque in laborum sapiente qui quibusdamx </p>
                     <p>Amet ipsum nesciunt corrupti odio adipisci Maxime eaque voluptas at repellendus laudantium perspiciatis eligendi perferendis? Vel voluptatem maxime quisquam nostrum nam modi. Voluptas asperiores labore tenetur repellat iusto Expedita asperiores. </p>
                 </div>
+                <div className="faq-line"></div>
             </div>
             <div className="question">
                 <h4>Daily Schedule</h4>
@@ -21,6 +23,7 @@ const FaqSection = () => {
                     <p>Dolor autem neque in laborum sapiente qui quibusdamx </p>
                     <p>Amet ipsum nesciunt corrupti odio adipisci Maxime eaque voluptas at repellendus laudantium perspiciatis eligendi perferendis? Vel voluptatem maxime quisquam nostrum nam modi. Voluptas asperiores labore tenetur repellat iusto Expedita asperiores. </p>
                 </div>
+                <div className="faq-line"></div>
             </div>
             <div className="question">
                 <h4>What else?</h4>
@@ -28,11 +31,45 @@ const FaqSection = () => {
                     <p>Dolor autem neque in laborum sapiente qui quibusdamx </p>
                     <p>Amet ipsum nesciunt corrupti odio adipisci Maxime eaque voluptas at repellendus laudantium perspiciatis eligendi perferendis? Vel voluptatem maxime quisquam nostrum nam modi. Voluptas asperiores labore tenetur repellat iusto Expedita asperiores. </p>
                 </div>
+                <div className="faq-line"></div>
             </div>
-        </About>
+        </Faq>
     )
 
 };
+
+const Faq = styled(About)`
+    display: block;  
+    span {
+        display: block;
+    }
+    h2 {
+        padding-bottom: 2rem;
+        font-weight: lighter;
+    }
+    .faq-line{
+        background: #ccc;
+        height: 0.2rem;
+        margin: 2rem 0rem;
+        width: 100%;
+    }
+    .question {
+        padding: 3rem 0rem;
+        cursor: pointer;
+    }
+    .answer {
+        padding: 2rem 0rem;
+        p{
+            padding: 1rem 0rem;
+        }
+    
+    .answer {
+        padding: 2rem 0rem;
+        p{
+            padding: 1rem 0rem;
+        }
+    }}
+`;
 
 export default FaqSection;
 
